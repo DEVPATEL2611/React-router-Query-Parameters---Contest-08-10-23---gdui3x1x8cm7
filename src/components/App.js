e473f043f4a32f29cd781f7c7a1c9bc741babc3c
Dev Patel <DEVVPATEL2611@GMAIL.COM>
2023-10-08 10:35:41 +0530

    ss

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/App.css";
-import { BrowserRouter as Router,Link, Route, Switch, useLocation } from "react-router-dom";
+import { BrowserRouter as Router,Link, Routes,Route, Switch, useLocation } from "react-router-dom";
 import Home from "./Home";
 import Shapes from "./Shapes";
 import Image from "./Image";
@@ -11,22 +11,33 @@ import Image from "./Image";
 export default function App() {
   return (
     
-    <div id="home">
+    <Router>
+      <div id="home">
       <nav className="navbar navbar-light">
         <ul className="nav navbar-nav">
           <li>
             {/* Home*/}
+            <Link to="/">Home</Link>
           </li>
           <li>
             {/* Shapes*/}
+            <Link to="/shapes">Shapes</Link>
           </li>
           <li>
            {/* Image*/}
+           <Link to="/image">Images</Link>
           </li>
         </ul>
       </nav>
-   
+      <Switch>
+          <Route path="/" Component={Home} />
+          <Route path="/shapes" Component={Shapes} />
+          <Route path="/image" Component={Image} />
+      </Switch>
+      
     </div>
+       
+    </Router>
       
   );
 }
